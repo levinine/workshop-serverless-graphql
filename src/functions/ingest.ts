ccedbee53e22bd2212bb7571ebdfcd1077d8830b
Vuk Stankovic <v.stankovic@levi9.com>
2019-09-24 17:29:48 +0200

    Add elasticMQ

--- src/functions/ingest.ts
+++ src/functions/ingest.ts
@@ -1,9 +1,14 @@
 import { Handler, Context, Callback } from "aws-lambda";
 
-export const handler: Handler = (
+export const handler: Handler = async (
     event: any,
     context: Context,
     callback: Callback
-): void => {
+): Promise<any> => {
     console.log('[INGEST]: ', event);
+    const response = {
+        StatusCode: 200,
+        Body: JSON.stringify({ hello: 'world' })
+    };
+    return response;
 };
--- src/functions/pull-data.ts
+++ src/functions/pull-data.ts
@@ -5,20 +5,23 @@ import { Lambda } from "aws-sdk";
 export const handler: Handler = async (event: any, context: Context, callback: Callback): Promise<any> => {
     const lambda = new Lambda();
     const url = process.env.API_URL + '/data/top/totalvolfull?limit=10&tsym=USD&api_key=' + process.env.API_KEY;
-    console.log(url);
     const apiData = await axios.get(url);
-    console.log(apiData.data);
-    const response = {
-        statusCode: 200,
-        body: JSON.stringify(apiData.data)
-    };
-    const result = await lambda.invoke({
-        FunctionName: 'ingest',
-        LogType: 'Tail',
-        InvocationType: 'RequestResponse',
-        Payload: JSON.stringify(apiData.data)
-    });
-
-    console.log(result);
-    return response;
+    try {
+        await axios.post('/ingest', {
+            data: apiData.data
+        }, {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+    } catch (e) {
+        return {
+            statusCode: 500,
+            body: JSON.stringify({
+                err: e.message,
+                stack: e,
+                error: 'Failed to invoke ingest lambda'
+            })
+        }
+    }
 };
