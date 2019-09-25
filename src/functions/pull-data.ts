2157a4729143b2ea4927d1895d7a590e7876407e
Vuk Stankovic <v.stankovic@levi9.com>
2019-09-25 09:28:39 +0200

    Push data to ingest lambda from data puller

--- src/functions/pull-data.ts
+++ src/functions/pull-data.ts
@@ -1,13 +1,12 @@
 import { Handler, Context, Callback } from 'aws-lambda';
 import axios from 'axios';
-import { Lambda } from "aws-sdk";
 
 export const handler: Handler = async (event: any, context: Context, callback: Callback): Promise<any> => {
-    const lambda = new Lambda();
+    const apiUrl = process.env.IS_OFFLINE ? process.env.LOCAL_URL : process.env.REMOTE_URL;
     const url = process.env.API_URL + '/data/top/totalvolfull?limit=10&tsym=USD&api_key=' + process.env.API_KEY;
     const apiData = await axios.get(url);
     try {
-        await axios.post('/ingest', {
+        await axios.post(  apiUrl + '/ingest', {
             data: apiData.data
         }, {
             headers: {
