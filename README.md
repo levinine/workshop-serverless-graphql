e1cac8a5c7a19f9e0d254909e8e7058294ebd7c6
Vuk Stankovic <v.stankovic@levi9.com>
2019-10-22 10:34:43 +0200

    feat: setup local environment

--- README.md
+++ README.md
@@ -30,3 +30,29 @@ Most of these will be managed through Serverless framework.
 ## What are we building
 
 ![system diagram](assets/system-diagram.png "System diagram")
+
+## Run locally 
+
+Run ElasticMQ server (Requires Java JDK)
+
+```bash
+java -jar assets/elasticmq-server-0.14.14.jar
+```
+
+Download local DynamoDB 
+
+```bash
+sls dynamodb install
+```
+
+Run local DynamoDB 
+
+```bash
+sls dynamodb start --migrate
+```
+
+Run serverless offline 
+
+```bash
+sls offline
+```
--- src/functions/pull-data.ts
+++ src/functions/pull-data.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 export const handler: Handler = async (event: any, context: Context, callback: Callback): Promise<any> => {
     const apiUrl = process.env.IS_OFFLINE ? process.env.LOCAL_URL : process.env.REMOTE_URL;
-    const url = process.env.API_URL + '/data/top/totalvolfull?limit=10&tsym=USD&api_key=' + process.env.API_KEY;
+    const url = process.env.API_URL + 'data/top/totalvolfull?limit=10&tsym=USD&api_key=' + process.env.API_KEY;
     const apiData = await axios.get(url);
     try {
         await axios.post(  apiUrl + '/ingest', {
