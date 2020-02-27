d4c799954075a20d10ec7144d180e20ad72b4fcb
Vuk Stankovic <v.stankovic@levi9.com>
2020-02-27 22:31:34 +0100

    feat: refactor pull data lambda

--- server/src/functions/pull-data.ts
+++ server/src/functions/pull-data.ts
@@ -1,36 +1,34 @@
-import { Handler, Context, Callback } from 'aws-lambda';
+import { Handler } from 'aws-lambda';
 import axios from 'axios';
+import {getResponse} from "../helpers/utils";
 
-export const handler: Handler = async (event: any, context: Context, callback: Callback): Promise<any> => {
-    const apiUrl = process.env.IS_OFFLINE ? process.env.LOCAL_URL : process.env.REMOTE_URL;
-    const url = process.env.API_URL + 'data/top/totalvolfull?limit=10&tsym=USD&api_key=' + process.env.API_KEY;
+const apiUrl = process.env.IS_OFFLINE ? process.env.LOCAL_URL : process.env.REMOTE_URL;
+const url = process.env.API_URL + 'data/top/totalvolfull?limit=10&tsym=USD&api_key=' + process.env.API_KEY;
+
+export const handler: Handler = async (): Promise<any> => {
     try {
-        const apiData = await axios.get(url);
-        console.log('sending data to ingest lambda at ', apiUrl + 'ingest');
-        const ingestResult = await axios.post(   apiUrl + 'ingest', {
-            data: apiData.data
-        }, {
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        });
-
+        const apiData = await fetchData(url);
+        const ingestResult = await pushDataToIngest(apiUrl, apiData);
         console.log('Ingest result: ', ingestResult);
-        return {
-            statusCode: 200,
-            body: JSON.stringify({
-                message: 'Data pushed to ingest'
-            })
-        };
+        return getResponse(200, { message: 'Data pushed to ingest' });
     } catch (e) {
         console.log(e);
-        return {
-            statusCode: 500,
-            body: JSON.stringify({
-                err: e.message,
-                stack: e,
-                error: 'Failed to invoke ingest lambda'
-            })
-        }
+        return getResponse(500, e);
     }
 };
+
+async function fetchData(url: string) {
+    const response = await axios.get(url);
+    return response.data;
+}
+
+async function pushDataToIngest(url: string, data: any) {
+    console.log('sending data to ingest lambda at ', apiUrl + 'ingest');
+    return await axios.post(   url + 'ingest', {
+        data: data
+    }, {
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+}
