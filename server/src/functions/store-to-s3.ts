4f74d7dd980c1b5439f021b2ad9a48bf595be7b8
Vuk Stankovic <v.stankovic@levi9.com>
2020-03-23 11:52:11 +0100

    fix: refactor storing to s3 bucket

--- server/src/functions/store-to-s3.ts
+++ server/src/functions/store-to-s3.ts
@@ -1,4 +1,4 @@
-import {DynamoDBStreamEvent, Handler} from "aws-lambda";
+import {AttributeValue, DynamoDBRecord, DynamoDBStreamEvent, Handler} from "aws-lambda";
 import {S3} from "aws-sdk";
 import {PutObjectRequest} from "aws-sdk/clients/s3";
 import axios from 'axios';
@@ -8,28 +8,46 @@ const s3 = new S3();
 export const handler: Handler = async (
     event: DynamoDBStreamEvent
 ): Promise<any> => {
-    if (event.Records[0].eventName === 'INSERT') {
-        console.log('Dynamodb event', event.Records[0]);
-        const currencies = event.Records[0].dynamodb.NewImage.currencies.L;
-        console.log('currencies: ', JSON.stringify(currencies));
-        for(let i = 0; i < currencies.length; i++) {
-            console.log('fetching image ', process.env.IMAGE_URL + currencies[i].M.CoinInfo.M.ImageUrl.S);
-            let base64Image = await getBase64(process.env.IMAGE_URL + currencies[i].M.CoinInfo.M.ImageUrl.S);
-            const uploadParams = {
-                Bucket: process.env.ORIGIN_BUCKET_NAME,
-                Body: base64Image,
-                Key: currencies[i].M.CoinInfo.M.Image.S,
-                ACL: 'authenticated-read',
-                ContentType: 'image/png',
-            } as PutObjectRequest;
-            console.log('Uploading to S3', uploadParams);
-            let response = await s3.putObject(uploadParams).promise();
-            console.log('Image uploaded', response);
-        }
+    for(let recordIterator = 0; recordIterator < event.Records.length; recordIterator++) {
+        await handleDynamodbRecord(event.Records[recordIterator]);
+    }
+};
+
+async function handleDynamodbRecord(record: DynamoDBRecord) {
+    if (record.eventName === 'INSERT') {
+        await handleDynamodbInsert(record);
     } else {
         console.log('Unhandled event');
     }
-};
+}
+
+async function handleDynamodbInsert(record: DynamoDBRecord) {
+    console.log('Dynamodb event', record);
+    const currencies = record.dynamodb.NewImage.currencies.L;
+    console.log('currencies: ', JSON.stringify(currencies));
+    for(let i = 0; i < currencies.length; i++) {
+        try {
+            console.log('fetching image ', process.env.IMAGE_URL + currencies[i].M.CoinInfo.M.ImageUrl.S);
+            let base64Image = await getBase64(process.env.IMAGE_URL + currencies[i].M.CoinInfo.M.ImageUrl.S);
+            let response = await uploadToS3(base64Image, currencies[i]);
+            console.log('Image was uploaded', response);
+        } catch (e) {
+            console.log('There was an error uploading image', e.message);
+        }
+    }
+}
+
+async function uploadToS3(base64Image: string, currency: AttributeValue) {
+    const uploadParams = {
+        Bucket: process.env.ORIGIN_BUCKET_NAME,
+        Body: base64Image,
+        Key: currency.M.CoinInfo.M.Image.S,
+        ACL: 'authenticated-read',
+        ContentType: 'image/png',
+    } as PutObjectRequest;
+    console.log('Uploading to S3', uploadParams);
+    return await s3.putObject(uploadParams).promise();
+}
 
 async function getBase64(url: string): Promise<any> {
     return axios
